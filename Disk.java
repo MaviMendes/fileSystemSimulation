0d69d4b34a4f4327fb5dd6ca1d4ec139deda684e
MaviMendes <mviviribero@gmail.com>
2022-07-14 08:40:46 -0300

    disk and manager draft

---
+++ Disk.java
@@ -0,0 +1,8 @@
+public class Disk {
+
+    // bloco de controle de arquivo - N   | persistido: N * 28 bytes
+    // mapa de bits - N  | N/8 bytes
+    // bloco de dados - M | persistido: M * 128 bytes
+    // mapa de bits - M | M/8 bytes
+    
+}
